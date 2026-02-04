2729bc818ed73c85b410997f9604e476b29b7e6d
Alex Lentz <alexlentz@Alexs-Mac-mini.lan>
2026-02-04 12:58:07 -0800

    Fix: Add missing Link import to page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useState } from 'react';
+import Link from 'next/link';
 import { Book, ReadingStatus } from '@/types/book';
 import { useBooks } from '@/hooks/useBooks';
 import Header from '@/components/Header';
